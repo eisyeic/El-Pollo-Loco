cba566fae5cb75c02bd1e37a13caf25aaa84bf0e
eisyeic <davideisenbarth@gmail.com>
2025-10-22 12:33:10 +0200

    restructuring and organizing world.class.js

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -1,19 +1,41 @@
 class World {
   character = new Character();
-  enemies = [
-    new Chicken(), 
-    new Chicken(), 
-    new Chicken()
-  ];
+  enemies = [new Chicken(), new Chicken(), new Chicken()];
+  clouds = [new Cloud()];
 
+  backgroundObjects = [new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0, 75)];
+
+  canvas;
   ctx;
 
   constructor(canvas) {
-    this.ctx = canvas.getContext('2d');
+    this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
     this.draw();
   }
 
+
   draw() {
-    this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.addToMap(this.character);
+    this.addObjectsToMap(this.clouds);
+    this.addObjectsToMap(this.enemies);
+    this.addObjectsToMap(this.backgroundObjects);
+
+    let self = this;
+    requestAnimationFrame(function () {
+      self.draw(); 
+    }); 
+  }
+  
+    addObjectsToMap(objects) {
+      objects.forEach((o) => {
+        this.addToMap(o);
+      });
+    }
+
+  addToMap(mo) {
+    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
   }
 }
