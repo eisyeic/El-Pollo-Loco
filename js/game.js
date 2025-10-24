97bbf7ba329062d0bf0654c616766e9c07685835
eisyeic <davideisenbarth@gmail.com>
2025-10-24 22:18:50 +0200

    improved animation and restructuring in levels

--- js/game.js
+++ js/game.js
@@ -29,3 +29,23 @@ window.addEventListener("keydown", (e) => {
   console.log(e);
   }
 );
+
+window.addEventListener("keyup", (e) => {
+  if (e.keyCode == 39) {
+    keyboard.RIGHT = false;
+  }
+  if (e.keyCode == 37) {
+    keyboard.LEFT = false;
+  }
+  if (e.keyCode == 38) {
+    keyboard.UP = false;
+  }
+  if (e.keyCode == 40) {
+    keyboard.DOWN = false;
+  }
+  if (e.keyCode == 32) {
+    keyboard.SPACE = false;
+  }
+  console.log(e);
+  }
+);
--- js/models/character.class.js
+++ js/models/character.class.js
@@ -2,14 +2,17 @@ class Character extends MovableObject {
   y = 180;
   height = 250;
   width = 150;
+  speed = 10;
   IMAGES_WALKING = [
-    'img/2_character_pepe/2_walk/W-21.png',
-    'img/2_character_pepe/2_walk/W-22.png',
-    'img/2_character_pepe/2_walk/W-23.png',
-    'img/2_character_pepe/2_walk/W-24.png',
-    'img/2_character_pepe/2_walk/W-25.png',
-    'img/2_character_pepe/2_walk/W-26.png'
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
   ];
+
+  world;
 
   constructor() {
     super().loadImage("img/2_character_pepe/1_idle/idle/I-1.png");
@@ -20,11 +23,26 @@ class Character extends MovableObject {
 
   animate() {
     setInterval(() => {
-    let i = this.currentImage % this.IMAGES_WALKING.length;
-    let path = this.IMAGES_WALKING[i];
-    this.img = this.imageCache[path];
-    this.currentImage++;
-    }, 100);
+      if (this.world.keyboard.RIGHT) {
+        this.x += this.speed;
+        this.otherDirection = false;
+      }
+
+      if (this.world.keyboard.LEFT) {
+        this.x -= this.speed;
+        this.otherDirection = true;
+      }
+      this.world.camera_x = -this.x;
+    }, 1000 / 60);
+
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+      }
+    }, 50);
   }
 
   jump() {}
