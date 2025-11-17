383d53e9faa4333100541339e141f27ceb87a50e
eisyeic <davideisenbarth@gmail.com>
2025-11-17 23:55:43 +0100

    events for buttons created and function stop events

--- js/game.js
+++ js/game.js
@@ -5,6 +5,74 @@ let keyboard = new Keyboard();
 function init() {
   canvas = document.getElementById("canvas");
   world = new World(canvas);
+  initTouchBtns();
+}
+
+function initTouchBtns() {
+  initBottleBtn();
+  initJUmpBtn();
+  initRightBtn();
+  initLeftBtn();
+}
+
+function initBottleBtn() {
+  const btnBottle = document.getElementById("btn-bottle");
+
+  if (btnBottle) {
+    btnBottle.addEventListener("touchstart", (e) => {
+      e.preventDefault();
+      keyboard.STRG = true;
+    });
+    btnBottle.addEventListener("touchend", (e) => {
+      e.preventDefault();
+      keyboard.STRG = false;
+    });
+  }
+}
+
+function initJUmpBtn() {
+  const btnJump = document.getElementById("btn-jump");
+
+  if (btnJump) {
+    btnJump.addEventListener("touchstart", (e) => {
+      e.preventDefault();
+      keyboard.SPACE = true;
+    });
+    btnJump.addEventListener("touchend", (e) => {
+      e.preventDefault();
+      keyboard.SPACE = false;
+    });
+  }
+}
+
+function initRightBtn() {
+  const btnRight = document.getElementById("btn-right");
+
+  if (btnRight) {
+    btnRight.addEventListener("touchstart", (e) => {
+      e.preventDefault();
+      keyboard.RIGHT = true;
+    });
+    btnRight.addEventListener("touchend", (e) => {
+      e.preventDefault();
+      keyboard.RIGHT = false;
+    });
+  }
+}
+
+function initLeftBtn() {
+  const btnLeft = document.getElementById("btn-left");
+
+  if (btnLeft) {
+    btnLeft.addEventListener("touchstart", (e) => {
+      e.preventDefault();
+      keyboard.LEFT = true;
+    });
+    btnLeft.addEventListener("touchend", (e) => {
+      e.preventDefault();
+      keyboard.LEFT = false;
+    });
+  }
 }
 
 window.addEventListener("keydown", (e) => {
@@ -44,7 +112,11 @@ window.addEventListener("keyup", (e) => {
   if (e.keyCode == 32) {
     keyboard.SPACE = false;
   }
-   if (e.keyCode == 17) {
+  if (e.keyCode == 17) {
     keyboard.STRG = false;
   }
 });
+
+function clearAllIntervals() {
+    for (let i = 1; i < 9999; i++) window.clearInterval(i);
+  }
--- js/models/throwable-oject.class.js
+++ js/models/throwable-oject.class.js
@@ -2,16 +2,14 @@ class ThrowableObject extends MovableObject {
 
     constructor(x, y) {
         super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
-        this.x = x;
-        this.y = y;
+        this.x = x -15;
+        this.y = y + 60;
         this.height = 60;
         this.width = 50;
-        this.throw(200, 300);
+        this.throw();
     }
 
     throw(x, y) {
-        this.x = x;
-        this.y = y;
         this.speedY = 30;
         this.applyGravity();
         setInterval(() => {
